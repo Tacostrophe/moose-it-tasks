92f1edbf06e671bf99dcc06cc831539b8dfe008c
Дмитрий Кутин <TacoKong@yandex.ru>
2023-03-14 13:30:51 +0300

    first attempt

---
+++ sprint_2/1_kinopostgres/kinopostgres.sql
@@ -0,0 +1,107 @@
+DROP DATABASE IF EXISTS kinopostgres;
+
+CREATE DATABASE kinopostgres;
+\connect kinopostgres
+
+CREATE TABLE kino_user (
+    id serial PRIMARY KEY,
+    nickname varchar(64) NOT NULL
+);
+
+CREATE TABLE country (
+    id serial PRIMARY KEY,
+    name varchar(64),
+    flag_path text
+);
+
+CREATE TABLE language (
+    id serial PRIMARY KEY,
+    name varchar(64)
+);
+
+CREATE TABLE person (
+    id serial PRIMARY KEY,
+    name varchar(64) NOT NULL,
+    country_id integer REFERENCES country
+);
+
+CREATE TABLE genre (
+    id serial PRIMARY KEY,
+    name varchar(64) NOT NULL
+);
+
+CREATE TABLE award (
+    id serial PRIMARY KEY,
+    name varchar(64) NOT NULL,
+    year integer CHECK (year > 1800)
+);
+
+CREATE TABLE film (
+    id serial PRIMARY KEY,
+    title text NOT NULL,
+    description text,
+    video_quality varchar(3) CHECK (video_quality IN ('SD', 'HD', 'FHD', 'QHD', '2K', '4K', '8K')),
+    poster_path text,
+    trailer_path text,
+    production_year integer CHECK (production_year > 1800),
+    production_country_id integer REFERENCES country,
+    slogan text,
+    director integer REFERENCES person,
+    screenwriter integer REFERENCES person,
+    producer integer REFERENCES person,
+    operator integer REFERENCES person,
+    composer integer REFERENCES person,
+    editor integer REFERENCES person,
+    budget money,
+    marketing money,
+    dvd_release date,
+    age_limit int CHECK( age_limit > 0),
+    mpaa_rating varchar(5) CHECK ( mpaa_rating IN ('G', 'PG', 'PG-13', 'R', 'NC-17')),
+    duration_min integer CHECK ( duration_min > 0)
+);
+
+CREATE TABLE film_user (
+    film_id integer REFERENCES film,
+    user_id integer REFERENCES kino_user,
+    rating integer NOT NULL CHECK (rating BETWEEN 1 AND 10),
+    review text,
+    PRIMARY KEY (film_id, user_id)
+);
+
+CREATE TABLE film_country (
+    film_id integer REFERENCES film,
+    country_id integer REFERENCES country,
+    fees integer CHECK (fees > 0),
+    viewers integer CHECK (viewers > 0),
+    premiere_date date
+);
+
+CREATE TABLE film_genre (
+    film_id integer REFERENCES film,
+    genre_id integer REFERENCES genre,
+    PRIMARY KEY (film_id, genre_id)
+);
+
+-- table of audio_tracks and subtitles to film
+CREATE TABLE film_language (
+    film_id integer REFERENCES film,
+    language_id integer REFERENCES language,
+    audio_track_path text,
+    subtitle_path text,
+    PRIMARY KEY (film_id, language_id),
+    CHECK (audio_track_path IS NOT NULL OR subtitle_path IS NOT NULL)
+);
+
+CREATE TABLE film_person (
+    film_id integer REFERENCES film,
+    person_id integer REFERENCES person,
+    dubbed boolean DEFAULT false,
+    PRIMARY KEY (film_id, person_id)
+);
+
+CREATE TABLE film_award (
+    film_id integer REFERENCES film,
+    award_id integer REFERENCES award,
+    nomination text,
+    PRIMARY KEY (film_id, award_id, nomination)
+);

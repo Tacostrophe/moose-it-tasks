e7487e350026806c56ff3c676fd85ca975a12001
Дмитрий Кутин <TacoKong@yandex.ru>
2023-03-15 20:02:33 +0300

    added ON DELETE to references

--- sprint_2/1_kinopostgres/kinopostgres.sql
+++ sprint_2/1_kinopostgres/kinopostgres.sql
@@ -22,7 +22,7 @@ CREATE TABLE language (
 CREATE TABLE person (
     id serial PRIMARY KEY,
     name varchar(64) NOT NULL,
-    country_id integer REFERENCES country
+    country_id integer REFERENCES country ON DELETE SET NULL
 );
 
 CREATE TABLE genre (
@@ -44,15 +44,15 @@ CREATE TABLE film (
     poster_path text,
     trailer_path text,
     production_year integer CHECK (production_year > 1800),
-    production_country_id integer REFERENCES country,
+    production_country_id integer REFERENCES country ON DELETE SET NULL,
     slogan text,
-    director_id integer REFERENCES person,
-    screenwriter_id integer REFERENCES person,
-    producer_id integer REFERENCES person,
-    operator_id integer REFERENCES person,
-    composer_id integer REFERENCES person,
-    artist_id integer REFERENCES person,
-    editor_id integer REFERENCES person,
+    director_id integer REFERENCES person ON DELETE SET NULL,
+    screenwriter_id integer REFERENCES person ON DELETE SET NULL,
+    producer_id integer REFERENCES person ON DELETE SET NULL,
+    operator_id integer REFERENCES person ON DELETE SET NULL,
+    composer_id integer REFERENCES person ON DELETE SET NULL,
+    artist_id integer REFERENCES person ON DELETE SET NULL,
+    editor_id integer REFERENCES person ON DELETE SET NULL,
     budget numeric,
     marketing numeric,
     dvd_release date,
@@ -62,31 +62,31 @@ CREATE TABLE film (
 );
 
 CREATE TABLE film_user (
-    film_id integer REFERENCES film,
-    user_id integer REFERENCES kino_user,
+    film_id integer REFERENCES film ON DELETE CASCADE,
+    user_id integer REFERENCES kino_user ON DELETE CASCADE,
     rating integer NOT NULL CHECK (rating BETWEEN 1 AND 10),
     review text,
     PRIMARY KEY (film_id, user_id)
 );
 
 CREATE TABLE film_country (
-    film_id integer REFERENCES film,
-    country_id integer REFERENCES country,
+    film_id integer REFERENCES film ON DELETE CASCADE,
+    country_id integer REFERENCES country ON DELETE CASCADE,
     fees numeric CHECK (fees > 0),
     viewers integer CHECK (viewers > 0),
     premiere_date date
 );
 
 CREATE TABLE film_genre (
-    film_id integer REFERENCES film,
-    genre_id integer REFERENCES genre,
+    film_id integer REFERENCES film ON DELETE CASCADE,
+    genre_id integer REFERENCES genre ON DELETE CASCADE,
     PRIMARY KEY (film_id, genre_id)
 );
 
 -- table of audio_tracks and subtitles to film
 CREATE TABLE film_language (
-    film_id integer REFERENCES film,
-    language_id integer REFERENCES language,
+    film_id integer REFERENCES film ON DELETE CASCADE,
+    language_id integer REFERENCES language ON DELETE CASCADE,
     audio_track_path text,
     subtitle_path text,
     PRIMARY KEY (film_id, language_id),
@@ -94,15 +94,15 @@ CREATE TABLE film_language (
 );
 
 CREATE TABLE film_person (
-    film_id integer REFERENCES film,
-    person_id integer REFERENCES person,
+    film_id integer REFERENCES film ON DELETE CASCADE,
+    person_id integer REFERENCES person ON DELETE CASCADE,
     dubbed boolean DEFAULT false,
     PRIMARY KEY (film_id, person_id)
 );
 
 CREATE TABLE film_award (
-    film_id integer REFERENCES film,
-    award_id integer REFERENCES award,
+    film_id integer REFERENCES film ON DELETE CASCADE,
+    award_id integer REFERENCES award ON DELETE CASCADE,
     nomination text,
     PRIMARY KEY (film_id, award_id, nomination)
 );

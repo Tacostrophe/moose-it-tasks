012545c988538c84fb85421a8e4849f5ca4af48a
Дмитрий Кутин <TacoKong@yandex.ru>
2023-04-02 22:45:12 +0300

    compatibility with users and roles

--- sprint_3/sprint_3/src/app.module.ts
+++ sprint_3/sprint_3/src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { User } from './users/users.model';
+import { UsersModule } from './users/users.module';
+import { RolesModule } from './roles/roles.module';
+import { Role } from './roles/roles.model';
+import { UserRole } from './roles/user_role.model';
 
 @Module({
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+    }),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: +process.env.POSTGRES_PORT,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      models: [User, Role, UserRole],
+      autoLoadModels: true,
+    }),
+    UsersModule,
+    RolesModule,
+  ],
 })
 export class AppModule {}
